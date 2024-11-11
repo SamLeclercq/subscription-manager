d6479b505005b4c28aa020db333ae80f8fd94365
Sam Leclercq <samleclercq.pro@gmail.com>
2024-11-11 18:30:30 +0100

    Annulation de l'url de base de donnée paramétrér dans un fichier .env initialement pour faciliter l'utilisation du projet à Monsieur RUPP-DAHLEM

--- src/Infrastructure/server.ts
+++ src/Infrastructure/server.ts
@@ -8,7 +8,6 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connexion à MongoDB
 mongoose.connect(`mongodb://localhost:27017/subscriptions`)
     .then(() => console.log('Connecté à MongoDB'))
     .catch((err: Error) => console.error('Erreur de connexion à MongoDB:', err));
